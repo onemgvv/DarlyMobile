c43c72eda8a5289ace819780c745a20080bce472
Mukhammad Gasanov <mukhammad.me@icloud.com>
2023-06-03 01:12:30 +0300

    Initial Commit

---
+++ Darly/DarlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarlyApp.swift
+//  Darly
+//
+//  Created by Magomed on 03.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct DarlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
